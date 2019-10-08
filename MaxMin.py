88b480537c541cddaa6afba20f39c6323c5eb9f5
Shekhar <rkshekhar015@gmail.com>
2019-10-08 17:51:27 +0530

    Palindrome in python

---
+++ MaxMin.py
@@ -0,0 +1,4 @@
+list1=[42,51,75,11]
+def diff(list1):
+    return max(list1)-min(list1)
+print(diff(list1))
---
+++ palindrome.py
@@ -0,0 +1,7 @@
+def palin(str1):
+    return str1[::-1]==str1
+    # if str1==l:
+        # return True
+    # return False
+str1=input("Enter a string :")
+print(palin(str1))
